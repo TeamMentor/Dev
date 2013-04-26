4048aa68a3b6781941106bcd7c1a6c2e535a28cc
Kofi sarfo <kofi@wimiro.com>
2013-04-26 16:17:09 +0100

    Added unit tests for web service

--- Web Applications/TeamMentor.UnitTests/REST_Direct/Test_REST_Admin.cs
+++ Web Applications/TeamMentor.UnitTests/REST_Direct/Test_REST_Admin.cs
@@ -28,38 +28,69 @@ namespace TeamMentor.UnitTests.REST
                 "Compiled OK: {0}".info(method.Name);                
             }
         }
+
         [Test] public void Invoke_O2_Script_Library()
         {
             var result = TmRest.Admin_InvokeScript("AAAAAAAAAA");            
             Assert.AreEqual("script not found", result);
             result = TmRest.Admin_InvokeScript("ping");            
             Assert.AreEqual("pong", result);
-            
         }
+
         [Test] public void SendEmail()
         {            
             var emailsSent_Before = SendEmails.Sent_EmailMessages.size();
-            var to                = "tm@si.com";
-            var subject           = "a subject";
-            var message           = "a message";
+            const string to      = "tm@si.com";
+            const string subject = "a subject";
+            const string message = "a message";
+
             var emailMessagePost  = new EmailMessage_Post 
                                         {
-                                            To    = to,
+                                            To = to,
                                             Subject = subject,
                                             Message = message
                                         };
+
             TmRest.SendEmail(emailMessagePost);
 
-            var sentMessages      = SendEmails.Sent_EmailMessages;
+            var sentMessages     = SendEmails.Sent_EmailMessages;
             var emailsSent_After = sentMessages.size();
             var lastMessage      = sentMessages.last();
    
-            Assert.IsTrue     (new SendEmails().serverNotConfigured());
-            Assert.Greater    (emailsSent_Before   , 0);
-            Assert.AreNotEqual(emailsSent_Before   , emailsSent_After);
-            Assert.AreEqual   (lastMessage.To      , emailMessagePost.To);
-            Assert.AreEqual   (lastMessage.Subject , emailMessagePost.Subject);
-            Assert.AreEqual   (lastMessage.Message , emailMessagePost.Message);
+            Assert.IsTrue           (new SendEmails().serverNotConfigured());
+            Assert.GreaterOrEqual   (emailsSent_Before, 0);
+            Assert.AreNotEqual      (emailsSent_Before   , emailsSent_After);
+            Assert.AreEqual         (lastMessage.To      , emailMessagePost.To);
+            Assert.AreEqual         (lastMessage.Subject , emailMessagePost.Subject);
+            Assert.AreEqual         (lastMessage.Message , emailMessagePost.Message);
+        }
+
+        [Test] public void SendFeedback()
+        {
+            var emailsSent_Before = SendEmails.Sent_EmailMessages.size();
+            var from              = "not-tm@not-si.com";
+            var subject           = "feedback subject";
+            var message           = "feedback message";
+
+            var feedbackMessagePost = new FeedbackMessage_Post
+            {
+                From = from,
+                Subject = subject,
+                Message = message
+            };
+
+            TmRest.SendFeedback(feedbackMessagePost);
+
+            var sentMessages = SendEmails.Sent_EmailMessages;
+            var emailsSent_After = sentMessages.size();
+            var lastMessage = sentMessages.last();
+
+            Assert.IsTrue           (new SendEmails().serverNotConfigured());
+            Assert.GreaterOrEqual   (emailsSent_Before, 0);
+            Assert.AreNotEqual      (emailsSent_Before, emailsSent_After);
+            Assert.AreEqual         (lastMessage.From, feedbackMessagePost.From);
+            Assert.AreEqual         (lastMessage.Subject, feedbackMessagePost.Subject);
+            Assert.AreEqual         (lastMessage.Message, feedbackMessagePost.Message);
         }
     }
 }
